737de499201fbe97af6684c534fb7ca72ae149f7
RachidJedata <jedatarachid@gmail.com>
2025-07-17 15:24:22 +0100

    Implement Posts Card Components

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import PostCard from '@/components/common/PostCard';
-import Header from '@/components/layout/Header';
-import Footer from '@/components/layout/Footer';
+import PostCard from "@/components/common/PostCard";
+import Header from "@/components/layout/Header";
+import { PostProps } from "@/interfaces";
 
-const PostsPage: React.FC = () => {
+const Posts: React.FC<PostProps[]> = (posts) => {
+  console.log(posts)
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
-        <h1 className="text-2xl font-bold mb-4">Posts</h1>
-        <PostCard title="Post Title" description="This is a sample post." />
+        <div className="flex justify-between">
+        <h1 className=" text-2xl font-semibold">Post Content</h1>
+        <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+        </div>
+        <div className="grid grid-cols-3 gap-2 ">
+          {
+            posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
+              <PostCard title={title} body={body} userId={userId} id={id} key={key} />
+            ))
+          }
+        </div>
       </main>
-      <Footer />
-    </>
-  );
-};
+    </div>
+  )
+}
 
-export default PostsPage;
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export default Posts;
